e6d28b474ac102f277809efa1eebecbb14957eb1
duxiaokong <1024114660@qq.com>
2020-11-19 17:11:30 +0800

    composer bug

---
+++ tests/test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 说明.
+ * User: dxk
+ * Date: 2020-11-19
+ */
+require '../vendor/autoload.php';
+$res = EasyHelper\Verify::isMobile('18510981152');
+echo(easy_curl_get('https://www.baidu.com'));
